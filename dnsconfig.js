ae81556d905b57322315dbb351468ef2ed5cba71
Klaas Pieter Annema <kp@annema.me>
2025-09-22 21:10:58 +0200

    Change Home Assistant setup
    It was managed through Cloudflare but I'm now using Nabu Casa's built-in custom
    domain functionality.

--- dnsconfig.js
+++ dnsconfig.js
@@ -18,22 +18,39 @@ D(
   DefaultTTL(300),
   SHARED_RECORDS,
   A("coolify", "91.99.116.152"),
-  A("home", "62.251.111.253"),
   A("test", "91.99.116.152"),
   A("www", "185.199.111.153"),
+
+  // Fastmail
   CNAME("fm1._domainkey", "fm1.annema.me.dkim.fmhosted.com."),
   CNAME("fm2._domainkey", "fm2.annema.me.dkim.fmhosted.com."),
   CNAME("fm3._domainkey", "fm3.annema.me.dkim.fmhosted.com."),
+
+  // Home Assistant
+  CNAME("home", "goxsf792guxzyy0ugkgrinr29bqbcfcy.ui.nabu.casa."),
+  CNAME(
+    "_acme-challenge.home",
+    "_acme-challenge.goxsf792guxzyy0ugkgrinr29bqbcfcy.ui.nabu.casa.",
+  ),
+
+  // hey.com
   TXT("@", "hey-verification:ao9ZVSWSAQadQo4W4TNw9eRp"),
+
+  // Google
   TXT(
     "@",
     "google-site-verification=zOJ2arCCTrNjCpBGXDbMznOHpC0nQmoTaQX5fA8VG34",
   ),
+
+  // haveibeenpwned.com
   TXT("@", "have-i-been-pwned-verification=477e35fb82ee40a7ee8bb53b27c36c94"),
+
   TXT(
     "_dmarc",
     '"v=DMARC1;  p=none; rua=mailto:d2ec14195c8044439a04e28b2d8efb62@dmarc-reports.cloudflare.net"',
   ),
+
+  // keybase.io
   TXT(
     "_keybase",
     "keybase-site-verification=I4BAM8jh_KxWKiR5jirkwfaXKq6fzGIRfVw-EMe28wE",
